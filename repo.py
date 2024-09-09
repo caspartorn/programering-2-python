f3648ec81b1d09eafcbc9552e6fdea70ea3744bf
caspar <caspar.tornving@skola.taby.se>
2024-09-09 13:59:55 +0200

    ny

--- repo.py
+++ repo.py
@@ -43,44 +43,45 @@
 
 # print("Number of elements in the resulting set:", len(result_set))
 
-class Person:
-    __weight = 0                    # privat attribut
+# class Person:
+#     __weight = 0                    # privat attribut
 
-    def setWeight(self, weight):    # publik setter-metod
-        if type(weight) == int and weight > 0:
-            self.__weight = weight
-        else:
-            print("Du måste ange ett positivt heltal!")
+#     def setWeight(self, weight):    # publik setter-metod
+#         if type(weight) == int and weight > 0:
+#             self.__weight = weight
+#         else:
+#             print("Du måste ange ett positivt heltal!")
 
-    def getWeight(self):		# publik getter-metod
-        return self.__weight
-
-
-p = Person()
-p.setWeight(-2)     			# Du måste ange ett positivt heltal
-p.setWeight(70)
-print(p.getWeight())
+#     def getWeight(self):		# publik getter-metod
+#         return self.__weight
 
 
-class elev:
-    def __init__(self, ålder, namn):
-        self.namn = namn
-        self.ålder = ålder
+# p = Person()
+# p.setWeight(-2)     			# Du måste ange ett positivt heltal
+# p.setWeight(70)
+# print(p.getWeight())
 
 
-elev1 = elev(15, "Anna")
-print(f"namn: {elev1.namn}, Ålder: {elev1.ålder}")
+# class elev:
+#     def __init__(self, ålder, namn):
+#         self.namn = namn
+#         self.ålder = ålder
 
 
-class Elev:
-    def __init__(self, namn, ålder, godkänd):
-        self.namn = namn
-        self.ålder = ålder
-        self.glad = godkänd
+# elev1 = elev(15, "Anna")
+# print(f"namn: {elev1.namn}, Ålder: {elev1.ålder}")
 
 
-elev1 = Elev("Anna", 15, True)
-print(f"Namn: {elev1.namn}, Ålder: {elev1.ålder}, Glad: {elev1.glad}")
+# class Elev:
+#     def __init__(self, namn, ålder, godkänd):
+#         self.namn = namn
+#         self.ålder = ålder
+#         self.glad = godkänd
 
-elev2 = Elev("Oskar", 16, False)
-print(f"Namn: {elev2.namn}, Ålder: {elev2.ålder}, Glad: {elev2.glad}")
+
+# elev1 = Elev("Anna", 15, True)
+# print(f"Namn: {elev1.namn}, Ålder: {elev1.ålder}, Glad: {elev1.glad}")
+
+# elev2 = Elev("Oskar", 16, False)
+# print(f"Namn: {elev2.namn}, Ålder: {elev2.ålder}, Glad: {elev2.glad}")
+#
