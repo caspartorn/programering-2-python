9df84ff7d2a383c454cfc657601be5558deae97e
caspar <caspar.tornving@skola.taby.se>
2024-09-09 14:55:46 +0200

    new

--- repo.py
+++ repo.py
@@ -85,3 +85,35 @@
 # elev2 = Elev("Oskar", 16, False)
 # print(f"Namn: {elev2.namn}, Ålder: {elev2.ålder}, Glad: {elev2.glad}")
 #
+
+# ---------------------------
+# |           Elev           |
+# ---------------------------
+# | - namn: str              |
+# | - ålder: int             |
+# | - glad: bool             |
+# ---------------------------
+# | + __init__(namn, ålder,  |
+# |            godkänd):     |
+# ---------------------------
+
+class Bil:
+    antalBilar = 0
+
+    def __init__(self, maxHastighet):
+        self.__maxHastighet = maxHastighet
+        Bil.antalBilar += 1
+
+    def get_maxHastighet(self):
+        return self.__maxHastighet
+
+
+bil1 = Bil(180)
+bil2 = Bil(220)
+bil3 = Bil(230)
+
+print(f"Bil 1 MaxHastighet: {bil1.get_maxHastighet()} km/h")
+print(f"Bil 2 MaxHastighet: {bil2.get_maxHastighet()} km/h")
+print(f"Bil 3 MaxHastighet: {bil3.get_maxHastighet()} km/h")
+
+print(f"Antal skapade bilar: {Bil.antalBilar}")
